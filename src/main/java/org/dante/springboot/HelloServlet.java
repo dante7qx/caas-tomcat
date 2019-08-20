1a024bb2802efa014c0794154d12be22f3945f6e
dante <sunchao.0129@163.com>
2019-08-20 18:30:25 +0800

    添加ingress

--- src/main/java/org/dante/springboot/HelloServlet.java
+++ src/main/java/org/dante/springboot/HelloServlet.java
@@ -1,25 +1,32 @@
 package org.dante.springboot;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
- * Servlet implementation class HelloServlet
+ * Log4j2与Slf4j的最佳实践
+ * https://zhuanlan.zhihu.com/p/36554554
  */
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private static Logger logger = LogManager.getLogger(HelloServlet.class.getName());
+	
     public HelloServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Log4j2 to record log...");
+		logger.warn("Log4j2 to record log...");
+		logger.error("Log4j2 to record log...");
 		response.setContentType("text/html; charset=UTF-8");
 		response.getWriter().append("<h1 align='center'>欢迎使用 CaaS 容器云！</h1>");
 	}
